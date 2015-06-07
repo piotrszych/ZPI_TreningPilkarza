c6782b2825656a535f9f72d5727e65d139c5b212
LittleMy <NauhgtyLittleMy@gmail.com>
2015-06-07 19:22:44 +0200

    -wyznaczenie wydajnosci uzytkownika, wymaganej wydajnosci, odleglosci

--- app/src/main/java/zip/android/treningpilkarski/SpecExcerciseFragment.java
+++ app/src/main/java/zip/android/treningpilkarski/SpecExcerciseFragment.java
@@ -6,12 +6,25 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.NumberPicker;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class SpecExcerciseFragment extends Fragment {
+
+    int iloscWykonanych = 9;
+    int ileDoWYkonania = 9;
+    int maxWykonanych = 10; // do pobrania z bazy
+    int odleglosc = 30; // do pobrania z bazy
+    TextView tv_twoja_wydajnosc;
+    TextView tv_wymagana_wydajnosc;
+    TextView tv_dystans;
+
+    NumberPicker picker;
 
 
     public SpecExcerciseFragment() {
@@ -22,8 +35,42 @@ public class SpecExcerciseFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_spec_excercise, container, false);
+        View view = inflater.inflate(R.layout.fragment_spec_excercise, container, false);
+
+        picker = (NumberPicker) view.findViewById(R.id.exercise_dialog_numberPicker);
+        tv_twoja_wydajnosc = (TextView) view.findViewById(R.id.exercise_effect);
+        tv_wymagana_wydajnosc = (TextView) view.findViewById(R.id.exercise_required);
+        tv_dystans = (TextView) view.findViewById(R.id.exercise_distance);
+        picker.setMinValue(0);
+        picker.setMaxValue((int)maxWykonanych);
+        picker.setValue(1);
+        picker.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker picker, int oldVal, int iloscWykonanych) {
+                double wydajnosc = ((double)iloscWykonanych / (double)maxWykonanych) * 100;
+                //Toast.makeText(getActivity().getApplicationContext(),"Wydajnosc: " + (int) wydajnosc,Toast.LENGTH_SHORT).show();
+                String message1 = "Twoja wydajność: " + wydajnosc + "%";
+                tv_twoja_wydajnosc.setText(message1);
+
+            }
+        });
+        wymaganaWydajnosc( ileDoWYkonania,  maxWykonanych);
+        odleglosc(odleglosc);
+
+        return view;
+
+    }
+    public void wymaganaWydajnosc(int ileDoWYkonania, int maxWykonanych){
+        double wymaganaWydajnosc = (int)(((double)ileDoWYkonania/(double)maxWykonanych) * 100);
+        String message2 = "Wymagana wydajność: " + wymaganaWydajnosc + "%";
+        tv_wymagana_wydajnosc.setText(message2);
+    }
+
+    public void odleglosc(int odleglosc){
+        String message3 = "Odległość: " + odleglosc + " m";
+        tv_dystans.setText(message3);
     }
 
 
