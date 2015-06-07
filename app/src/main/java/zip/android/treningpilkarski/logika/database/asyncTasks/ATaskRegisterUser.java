42481ab32ac950c95ab8b7cc27b5233145280c9e
piotrszych <piotrszych@hotmail.com>
2015-06-08 01:17:58 +0200

    Naprawiono: ATaskRegisterUser nie dodaje nowych cwiczen, gdy user jest juz w bazie

--- app/src/main/java/zip/android/treningpilkarski/logika/database/asyncTasks/ATaskRegisterUser.java
+++ app/src/main/java/zip/android/treningpilkarski/logika/database/asyncTasks/ATaskRegisterUser.java
@@ -84,34 +84,35 @@ import zip.android.treningpilkarski.logika.database.interfaces.ICommWithDB;
                 int i_success = json.getInt(TAG_SUCCESS);
                 toReturn = i_success;
 
-                List<NameValuePair> params_internal = new ArrayList<NameValuePair>();
-                String url_internal = "http://" + DataKeys.S_DATABASE_IP_ADDRESS + "/bazaphp/read_exercises_by_position.php";
-                params_internal.add(new BasicNameValuePair("pozycja", "" + position));
-                JSONObject json_internal = _json_parser.makeHttpRequest(url_internal, "GET", params_internal);
-                int i_success_internal = json_internal.getInt(TAG_SUCCESS);
-                if(i_success_internal == 1)
-                {
-                    JSONArray jsonArr = json_internal.getJSONArray("cwiczenie_pozycji");
-                    Log.d("Info", "Yoo" + jsonArr.toString());
-                    List<Integer> exercisesList = new ArrayList<>();
-                    //int[] array = null;
-                    Object[] exercisesArray = exercisesList.toArray();
-                    List<NameValuePair> exercisesNVP = new ArrayList<NameValuePair>();
-                    String urlExercises = "http://" + DataKeys.S_DATABASE_IP_ADDRESS + "/bazaphp/insert_first_exercises.php";
-                    for(int i = 0; i < jsonArr.length(); i++){
-                        int exercise = jsonArr.getJSONObject(i).getInt("id_cwiczenia");
-                        exercisesNVP.add(new BasicNameValuePair("cwiczenia[]", "" + exercise));
+                if(i_success == 1) {
+                    List<NameValuePair> params_internal = new ArrayList<NameValuePair>();
+                    String url_internal = "http://" + DataKeys.S_DATABASE_IP_ADDRESS + "/bazaphp/read_exercises_by_position.php";
+                    params_internal.add(new BasicNameValuePair("pozycja", "" + position));
+                    JSONObject json_internal = _json_parser.makeHttpRequest(url_internal, "GET", params_internal);
+                    int i_success_internal = json_internal.getInt(TAG_SUCCESS);
+                    if (i_success_internal == 1) {
+                        JSONArray jsonArr = json_internal.getJSONArray("cwiczenie_pozycji");
+                        Log.d("Info", "Yoo" + jsonArr.toString());
+                        List<Integer> exercisesList = new ArrayList<>();
+                        //int[] array = null;
+                        Object[] exercisesArray = exercisesList.toArray();
+                        List<NameValuePair> exercisesNVP = new ArrayList<NameValuePair>();
+                        String urlExercises = "http://" + DataKeys.S_DATABASE_IP_ADDRESS + "/bazaphp/insert_first_exercises.php";
+                        for (int i = 0; i < jsonArr.length(); i++) {
+                            int exercise = jsonArr.getJSONObject(i).getInt("id_cwiczenia");
+                            exercisesNVP.add(new BasicNameValuePair("cwiczenia[]", "" + exercise));
+                        }
+                        exercisesNVP.add(new BasicNameValuePair("login", s_login));
+                        JSONObject jsonReq = _json_parser.makeHttpRequest(urlExercises, "POST", exercisesNVP);
+                        int success = jsonReq.getInt(TAG_SUCCESS);
+                        Log.d("SUKCES", success + "");
+                        //System.out.println(jsonArr.toString());
+                        //JSONObject tempJSONObjectInternal = jsonArrayInternal.getJSONObject(0);
+                        //int i_previous_exercise = Integer.parseInt(tempJSONObjectInternal.getString("ilosc_wykonanych"));
+                        //Log.d("PrevExerciseID got", ""+i_previous_exercise);
+                        //_hashmap_toreturn.put("Previous", i_previous_exercise);
+                        //_hashmap_toreturn.put("ExerID", i_exercise_id);     //TODO zwracac poprawne ID
                     }
-                    exercisesNVP.add(new BasicNameValuePair("login", s_login));
-                    JSONObject jsonReq = _json_parser.makeHttpRequest(urlExercises, "POST", exercisesNVP);
-                    int success = jsonReq.getInt(TAG_SUCCESS);
-                    Log.d("SUKCES", success + "");
-                    //System.out.println(jsonArr.toString());
-                    //JSONObject tempJSONObjectInternal = jsonArrayInternal.getJSONObject(0);
-                    //int i_previous_exercise = Integer.parseInt(tempJSONObjectInternal.getString("ilosc_wykonanych"));
-                    //Log.d("PrevExerciseID got", ""+i_previous_exercise);
-                    //_hashmap_toreturn.put("Previous", i_previous_exercise);
-                    //_hashmap_toreturn.put("ExerID", i_exercise_id);     //TODO zwracac poprawne ID
                 }
 
                 /*if (i_success == 1) {
