24dd3d5a21d05f06bc23bc2ed54e622f88cb178c
piotrszych <piotrszych@hotmail.com>
2015-06-14 18:14:54 +0200

    Dodano sprawdzenie poprzednich, niewykonanych ćwiczeń

--- app/src/main/java/zip/android/treningpilkarski/MainActivity.java
+++ app/src/main/java/zip/android/treningpilkarski/MainActivity.java
@@ -20,10 +20,12 @@ import java.util.Locale;
 
 import zip.android.treningpilkarski.logika.DataKeys;
 import zip.android.treningpilkarski.logika.DataProvider;
+import zip.android.treningpilkarski.logika.database.asyncTasks.ATaskGetExercisesNotDone;
+import zip.android.treningpilkarski.logika.database.interfaces.ICommWithDB;
 import zip.android.treningpilkarski.main_fragments.InfoFragment;
 import zip.android.treningpilkarski.main_fragments.ListFragment;
 
-public class MainActivity extends ActionBarActivity implements ActionBar.TabListener {
+public class MainActivity extends ActionBarActivity implements ActionBar.TabListener, ICommWithDB<Integer> {
 
     SectionsPagerAdapter mSectionsPagerAdapter;
     InfoFragment frag_infoFragment;
@@ -43,41 +45,9 @@ public class MainActivity extends ActionBarActivity implements ActionBar.TabList
 
         sharedPreferences = getSharedPreferences(DataKeys.S_SHAREDPREFERENCES_NAME_USER, Context.MODE_PRIVATE);
 
-        // Set up the action bar.
-        final ActionBar actionBar = getSupportActionBar();
-        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-        actionBar.setDisplayShowTitleEnabled(false);
-
-        // Create the adapter that will return a fragment for each of the three
-        // primary sections of the activity.
-        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager(), getIntent().getBundleExtra(DataKeys.BUNDLE_NAME_USER));
-        frag_infoFragment = new InfoFragment();
-
-        // Set up the ViewPager with the sections adapter.
-        mViewPager = (ViewPager) findViewById(R.id.pager);
-        mViewPager.setAdapter(mSectionsPagerAdapter);
-
-        // When swiping between different sections, select the corresponding
-        // tab. We can also use ActionBar.Tab#select() to do this if we have
-        // a reference to the Tab.
-        mViewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
-            @Override
-            public void onPageSelected(int position) {
-                actionBar.setSelectedNavigationItem(position);
-            }
-        });
-
-        // For each of the sections in the app, add a tab to the action bar.
-        for (int i = 0; i < mSectionsPagerAdapter.getCount(); i++) {
-            // Create a tab with text corresponding to the page title defined by
-            // the adapter. Also specify this Activity object, which implements
-            // the TabListener interface, as the callback (listener) for when
-            // this tab is selected.
-            actionBar.addTab(
-                    actionBar.newTab()
-                            .setText(mSectionsPagerAdapter.getPageTitle(i))
-                            .setTabListener(this));
-        }
+        int user_id = getIntent().getBundleExtra(DataKeys.BUNDLE_NAME_USER).getInt(DataKeys.BUNDLE_KEY_USERID);
+        ATaskGetExercisesNotDone atask_checkNotDone = new ATaskGetExercisesNotDone(this, this, user_id);
+        atask_checkNotDone.execute();
     }
 
     @Override
@@ -142,6 +112,51 @@ public class MainActivity extends ActionBarActivity implements ActionBar.TabList
             Toast.makeText(getBaseContext(), getString(R.string.exit_warning), Toast.LENGTH_SHORT).show();
             back_pressed = System.currentTimeMillis();
         }
+    }
+
+    private void loadPagerAdapter()
+    {
+// Set up the action bar.
+        final ActionBar actionBar = getSupportActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+        actionBar.setDisplayShowTitleEnabled(false);
+
+        // Create the adapter that will return a fragment for each of the three
+        // primary sections of the activity.
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager(), getIntent().getBundleExtra(DataKeys.BUNDLE_NAME_USER));
+        //frag_infoFragment = new InfoFragment();
+
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.pager);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+
+        // When swiping between different sections, select the corresponding
+        // tab. We can also use ActionBar.Tab#select() to do this if we have
+        // a reference to the Tab.
+        mViewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+            @Override
+            public void onPageSelected(int position) {
+                actionBar.setSelectedNavigationItem(position);
+            }
+        });
+
+        // For each of the sections in the app, add a tab to the action bar.
+        for (int i = 0; i < mSectionsPagerAdapter.getCount(); i++) {
+            // Create a tab with text corresponding to the page title defined by
+            // the adapter. Also specify this Activity object, which implements
+            // the TabListener interface, as the callback (listener) for when
+            // this tab is selected.
+            actionBar.addTab(
+                    actionBar.newTab()
+                            .setText(mSectionsPagerAdapter.getPageTitle(i))
+                            .setTabListener(this));
+        }
+    }
+
+    @Override
+    public void notifyActivity(Integer objectSent) {
+//        Toast.makeText(this, "Got in main: " + objectSent, Toast.LENGTH_SHORT).show();
+        loadPagerAdapter();
     }
 
     public class SectionsPagerAdapter extends FragmentPagerAdapter {
